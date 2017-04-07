f5d9d792974be6ea9e6bc95d4989db13be86d75a
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2017-04-06 20:05:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# theme_greatfulmed
+
+Odoo 10 custom theme for (www.greatfulmed.com)[https://www.greatfulmed.com]
+
+Great Med Cannabis Society  
+Vancouver, BC
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Greatful Med theme',
+    'description': 'Greatful Med Cannabis Society, Vancouver, BC.',
+    'version': '1.0',
+    'author': 'Derek Marcinyshyn',
+    'website': 'https://www.monasheemountainmultimedia.com',
+
+    'data': [
+        'views/theme_greatfulmed_templates.xml',
+        'views/assets.xml'
+    ],
+    'category': 'Theme/Ecommerce',
+    'depends': ['website', 'sale'],
+    'application': True,
+}
